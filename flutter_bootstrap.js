7c8354e28a1f7d46ee3fc54f7805fd61dd04df56
PavankumarSwamy Shesetti <pavanstarkin@gmail.com>
2026-09-06 19:09:31 +0530

    deploy: update GitHub Pages with Demo Fill and Google Auth updates

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"587c18f873b8ab57330422bce09047420d9c7f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1408666811"
+    serviceWorkerVersion: "2602264020"
   }
 });
