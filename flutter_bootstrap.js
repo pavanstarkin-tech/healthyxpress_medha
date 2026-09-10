049ce4525b2ebd750da8cb3dce0e810c18ecf873
PavankumarSwamy Shesetti <pavanstarkin@gmail.com>
2026-09-10 07:18:42 +0530

    deploy: update GitHub Pages with latest app and admin panel

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"587c18f873b8ab57330422bce09047420d9c7f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2199342881"
+    serviceWorkerVersion: "4046259799"
   }
 });
